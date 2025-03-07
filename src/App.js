752ddf09077c08ac9109a4c3ebd014722a81ea7f
Sirawitsptw <152370451+Sirawitsptw@users.noreply.github.com>
2025-03-08 01:14:52 +0700

    3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Lanyard from './Lanyard';
+import Homepage from './้homepage';
 
 export default function App() {
   return (
     <div className="body">
+      <Homepage />
       <Lanyard position={[0, 0, 20]} gravity={[0, -40, 0]} />
     </div>
   );
---
+++ src/้homepage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './homepage.css';
+
+export default function Homepage() {
+    return(
+        <>
+            <div className = 'text1'>HI I'm Sirawit Sriprathumwong <br /> I am study at KMUTNB</div>
+        </>
+    )
+}
