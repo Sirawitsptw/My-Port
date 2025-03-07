5df05e3468af52192a42c1d576a3856ee40436d2
Sirawitsptw <152370451+Sirawitsptw@users.noreply.github.com>
2025-03-07 23:05:24 +0700

    2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import Lanyard from './Lanyard';
-import Particles from './Particles';
 
 export default function App() {
   return (
     <div className="body">
-      <Particles />
       <Lanyard position={[0, 0, 20]} gravity={[0, -40, 0]} />
     </div>
   );
